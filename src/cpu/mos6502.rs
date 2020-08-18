c5ee02a89d6557871c3720c176d2c574ca207435
Stephan König <tb@octoplex.org>
2020-08-18 15:54:24 +0200

    x

--- src/cpu/mos6502.rs
+++ src/cpu/mos6502.rs
@@ -17,7 +17,6 @@ bitflags! {
     }
 }
 
-/*
 #[derive(Debug)]
 enum Addressing {
     Implicit,
@@ -33,7 +32,7 @@ enum Addressing {
     AbsIndexedY,
     IndexedIndirectX,
     IndexedIndirectY
-}*/
+}
 
 pub struct Mos6502 {
     a: u8,
@@ -45,11 +44,26 @@ pub struct Mos6502 {
     cycle: u32
 }
 
+macro_rules! a_imm {
+    ($this:ident, $bus:expr) => { $bus.read_u8($this.pc.wrapping_add(1)) }
+}
+
+macro_rules! a_abs16 {
+    ($this:ident, $bus:expr) => { ($bus.read_u8($this.pc.wrapping_add(2)) as u16) << 8 | $bus.read_u8($this.pc.wrapping_add(1)) as u16 }
+}
+
+macro_rules! flags_nz {
+    ($this:ident, $value:expr) => {
+        $this.p.set(Status::ZERO, $value == 0);
+        $this.p.set(Status::NEGATIVE, ($value & 0x80) == 0x80);
+    }
+}
+
 impl fmt::Display for Mos6502 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f,
-            "A={:#04x}({}) X={:#04x}({}) Y={:#04x}({}) S={:#04x}({}) FLAGS={:#04x}({:?}) PC={:#06x} CYCLE={}",
-            self.a, self.a, self.x, self.x, self.y, self.y, self.s, self.s, self.p, self.p, self.pc, self.cycle
+            "A={:02X} X={:02X} Y={:02X} S={:02X} FLAGS={:#04X}({:?}) PC={:#06X} CYCLE={}",
+            self.a, self.x, self.y, self.s, self.p, self.p, self.pc, self.cycle
         )
     }
 }
@@ -65,19 +79,39 @@ impl Mos6502 {
 
     pub fn reset(&mut self, bus: &mut dyn sys::MemoryAccessA16D8) {
         self.a = 0;
-        self.x = 0;
+        self.x = 1;
         self.y = 0;
         self.s = 0xfd;
         self.p.bits = 0x34;
         self.pc = (bus.read_u8(0xfffd) as u16) << 8 | bus.read_u8(0xfffc) as u16;
+        self.pc = 0xC000;
         self.cycle = 0;
+    }
+
+    pub fn debug_op(&self, bus: &mut dyn sys::MemoryAccessA16D8, op: u8) -> (u16, String) {
+        match op {
+            0x4c /* JMP abs */ => { (3, format!("JMP ${:04X}", a_abs16!(self, bus))) }
+            0x78 /* SEI */ => { (1, format!("SEI")) }
+            0xd8 /* CLD */ => { (1, format!("CLD")) }
+            0xa2 /* LDX #xx */ => { (2, format!("LDX #${:02X}", bus.read_u8(self.pc.wrapping_add(1)))) }
+            _ => { panic!("Invalid opcode {:#04x} [ {} ]", op, self); }
+        }
+    }
+
+    pub fn exec_op(&mut self, bus: &mut dyn sys::MemoryAccessA16D8, op: u8) -> u32 {
+        match op {
+            0x4c /* JMP abs */ => { self.pc = a_abs16!(self, bus); 3 }
+            0x78 /* SEI */ => { self.p.insert(Status::INT_DIS); self.pc = self.pc.wrapping_add(1); 2 }
+            0xd8 /* CLD */ => { self.p.remove(Status::DECIMAL); self.pc = self.pc.wrapping_add(1); 2 }
+            0xa2 /* LDX imm */ => { self.x = a_imm!(self, bus); flags_nz!(self, self.x); self.pc = self.pc.wrapping_add(2); 2 }
+            _ => { panic!("Invalid opcode {:#04x} [ {} ]", op, self); }
+        }
     }
     
     pub fn step(&mut self, bus: &mut dyn sys::MemoryAccessA16D8) {
-        let opcode = bus.read_u8(self.pc);
-        println!("opcode {:#04x}", opcode);
-
-        //self.pc = self.pc.wrapping_add(1);
-        self.cycle = self.cycle + 1;
+        let op = bus.read_u8(self.pc);
+        let (_, s) = self.debug_op(bus, op);
+        println!("{:#06X} {}", self.pc, s);
+        self.cycle = self.cycle + self.exec_op(bus, op);
     }
 }
